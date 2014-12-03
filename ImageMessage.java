e1e89b9d3a643edadd35408b2f01db30c56f1bc9
PanPanda <panzheng19881004@gmail.com>
2014-12-03 11:14:01 +0800

    Create ImageMessage.java

---
+++ ImageMessage.java
@@ -0,0 +1,24 @@
+package org.panzheng.course.message.req;
+
+
+/**
+ * 图片消息
+ * 
+ * 
+ * @author PanZheng
+ *
+ */
+public class ImageMessage extends BaseMessage {
+	//图片链接
+	private String PicUrl;
+
+	public String getPicUrl() {
+		return PicUrl;
+	}
+
+	public void setPicUrl(String picUrl) {
+		this.PicUrl = picUrl;
+	}
+
+
+}
