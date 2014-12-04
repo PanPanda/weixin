b6de76a5fab015478d4be3bbedcbc75f602de6fe
PanPanda <panzheng19881004@gmail.com>
2014-12-04 15:22:25 +0800

    Create MusicMessage.java

---
+++ MusicMessage.java
@@ -0,0 +1,22 @@
+package org.panzheng.course.message.resp;
+
+/**
+ * 音乐消息
+ * 
+ * @author PanZheng
+ *
+ */
+public class MusicMessage extends BaseMessage{
+	//音乐
+	private Music Music;
+
+	public Music getMusic() {
+		return Music;
+	}
+
+	public void setMusic(Music music) {
+		Music = music;
+	}
+	
+
+}
